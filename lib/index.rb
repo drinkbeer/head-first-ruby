11dcfb1d55ad86b504b45253c27f54df0df9038c
Jianbin Chen <jason.chen@shopify.com>
2022-03-31 18:27:17 -0700

    Update the app

--- lib/index.rb
+++ lib/index.rb
@@ -32,8 +32,8 @@ require "redis"
 #   client.close
 # }
 
-puts "ENV['REDIS_HOST']: #{ENV['REDIS_HOST']}"
-puts "ENV['REDIS_PORT']: #{ENV['REDIS_PORT']}"
+# puts "ENV['REDIS_HOST']: #{ENV['REDIS_HOST']}"
+# puts "ENV['REDIS_PORT']: #{ENV['REDIS_PORT']}"
 
 # while session = server.accept
 #     request = session.gets
@@ -55,9 +55,12 @@ require 'rack/lobster'
 require "logger"
 
 
-app = Rack::Lobster.new
+# app = Rack::Lobster.new
+app = Proc.new do
+  ['200', {'Content-Type' => 'text/html'}, ["Hello Redis and Ruby! The time is #{Time.now} \r\n"]]
+end
 server = TCPServer.new 8080
-logger = Logger.new("/var/log/ruby.log")
+logger = Logger.new(STDOUT)
 
 logger.info("Starting the server, ENV['REDIS_HOST']: #{ENV['REDIS_HOST']}, ENV['REDIS_PORT']: #{ENV['REDIS_PORT']}")
 # redis = Redis.new(host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT'], db: 15)
@@ -65,35 +68,60 @@ redis = Redis.new(:host => ENV["REDIS_HOST"] || "127.0.0.1" , :port => ENV["REDI
 
 while session = server.accept
   request = session.gets
-  puts request
+  # puts request
+  logger.info("request: #{request}")
  
   # 1
   method, full_path = request.split(' ')
   # 2
   path, query = full_path.split('?')
+  path = path[1..-1]
  
   # 3
-  status, headers, body = app.call({
-    'REQUEST_METHOD' => method,
-    'PATH_INFO' => path,
-    'QUERY_STRING' => query
-  })
+  # status, headers, body = app.call({
+  #   'REQUEST_METHOD' => method,
+  #   'PATH_INFO' => path,
+  #   'QUERY_STRING' => query
+  # })
+  status, headers, body = app.call({})
 
-  puts "path: #{path[1..-1]}, query: #{query}"
-  logger.info("path: #{path[1..-1]}, query: #{query}")
+  # puts "path: #{path[1..-1]}, query: #{query}"
+  logger.info("method: #{method}, path: #{path}, query: #{query}, status: #{status}, headers: #{headers}, body: #{body}")
 
-  redis.ping
-  redis.set("key", "value")
-  logger.info("GET key: #{redis.get("key")}")
+  # redis.ping
+  # redis.set("key", "value")
+  # logger.info("Set key successful. GET key: #{redis.get("key")}")
  
   session.print "HTTP/1.1 #{status}\r\n"
   headers.each do |key, value|
-    session.print "#{key}: #{value}\r\n"
+    session.puts "#{key}: #{value}\r\n"
   end
   session.print "\r\n"
   body.each do |part|
-    session.print part
+    session.puts part
   end
+
+  str = ""
+  if path.empty?
+    redis.ping
+    str = "PING redis successful!"
+  else
+    if path == "GET"
+      result = redis.get(query)
+      if result.nil?
+        str = "GET #{query} successful! The key doesn't exist in redis."
+      else
+        str = "GET #{query} successful! The value: #{result}"
+      end
+    else
+      if path == "SET"
+        key, value = query.split("=")
+        redis.set(key, value)
+        str = "SET key #{key}, value #{value} successful!"
+      end
+    end
+  end
+  session.puts "\n     #{str}"
   session.close
 end
 
