110dfd72d66ecad8e51b7e54b7000c5220bfa84d
Jason Chen <chenjb90@gmail.com>
2024-02-27 14:36:36 -0800

    Update the lock contention script

--- benchmark_mysql/bench_lock_contention.rb
+++ benchmark_mysql/bench_lock_contention.rb
@@ -5,12 +5,9 @@ def create_client
   Mysql2::Client.new(host: "localhost", username: "root", database: "benchmark")
 end
 
-def reset_table(client)
-  client.query("UPDATE test_table SET available = 100")
-end
-
 def execute_query(client)
-  result = client.query("SELECT id, available FROM test_table WHERE available > 0 LIMIT 1")
+    test_num = 0;
+  result = client.query("SELECT id, available FROM test_table WHERE test_num = #{test_num} AND available > 0 LIMIT 1")
   unless result.first
     return false # Indicate no rows found
   end
@@ -20,12 +17,14 @@ def execute_query(client)
   true
 end
 
+# The current model
 def execute_query_in_transaction(client)
+    test_num = 1;
     client.query("START TRANSACTION")
-    result = client.query("SELECT id, available FROM test_table WHERE available > 0 LIMIT 1")
+    result = client.query("SELECT id, available FROM test_table WHERE test_num = #{test_num} AND available > 0 LIMIT 1")
     unless result.first
       client.query("ROLLBACK")
-      return false # Indicate no rows found
+      return false
     end
     id = result.first["id"]
     available = result.first["available"]
@@ -33,13 +32,31 @@ def execute_query_in_transaction(client)
     client.query("COMMIT")
     true
 end
-  
+
+# The current model with SKIP LOCKED
 def execute_query_skip_locked(client)
+    test_num = 2;
     client.query("START TRANSACTION")
-    result = client.query("SELECT id, available FROM test_table WHERE available > 0 LIMIT 1 FOR UPDATE SKIP LOCKED")
+    result = client.query("SELECT id, available FROM test_table WHERE test_num = #{test_num} AND available > 0 LIMIT 1 FOR UPDATE SKIP LOCKED")
     unless result.first
       client.query("ROLLBACK")
-      return false # Indicate no rows found
+      return false
+    end
+    id = result.first["id"]
+    available = result.first["available"]
+    client.query("UPDATE test_table SET available = #{available - 1} WHERE id = #{id}")
+    client.query("COMMIT")
+    true
+end
+
+# The new model that have each variant item in one row, for example, if we have 3 variants, each have 100 available, then we have 300 rows
+def execute_query_skip_locked_per_row(client)
+    test_num = 3;
+    client.query("START TRANSACTION")
+    result = client.query("SELECT id, available FROM test_table WHERE test_num = #{test_num} AND available > 0 LIMIT 1 FOR UPDATE SKIP LOCKED")
+    unless result.first
+      client.query("ROLLBACK")
+      return false
     end
     id = result.first["id"]
     available = result.first["available"]
@@ -63,15 +80,15 @@ end
 Benchmark.bm do |x|
   client = create_client
   x.report("baseline") {
-    reset_table(client) # Reset table before the test
     run_task_in_threads("baseline") { |client| execute_query(client) }
   }
   x.report("query_in_transaction") {
-    reset_table(client) # Reset table before the test
     run_task_in_threads("query_in_transaction") { |client| execute_query_in_transaction(client) }
   }
   x.report("query_skip_locked") {
-    reset_table(client) # Reset table before the test
     run_task_in_threads("query_skip_locked") { |client| execute_query_skip_locked(client) }
+  }
+  x.report("query_skip_locked_per_row") {
+    run_task_in_threads("query_skip_locked_per_row") { |client| execute_query_skip_locked_per_row(client) }
   }
 end
--- benchmark_mysql/init.rb
+++ benchmark_mysql/init.rb
@@ -3,9 +3,67 @@ require 'mysql2'
 # Connect to the MySQL database
 client = Mysql2::Client.new(host: "localhost", username: "root", database: "benchmark")
 
-1.times do
-    random_number = rand(1..10000) # Adjust the range as needed
+(1..2000).each do |i|
+    row = client.query("SELECT * FROM test_table WHERE id = #{i}").first
+    if row
+        client.query("DELETE FROM test_table WHERE id = #{i}")
+    end
+
+    random_number = rand(1..10000)
     priority_value = "priority#{random_number}"
-    client.query("INSERT INTO test_table (priority) VALUES ('#{priority_value}')")
+    available = 100;
+    test_num = 0;
+    client.query("INSERT INTO test_table (id, priority, available, test_num) VALUES (#{i}, '#{priority_value}', #{available}, #{test_num})")
 end
 
+(202001..204000).each do |i|
+    row = client.query("SELECT * FROM test_table WHERE id = #{i}").first
+    if row
+        client.query("DELETE FROM test_table WHERE id = #{i}")
+    end
+
+    random_number = rand(1..10000)
+    priority_value = "priority#{random_number}"
+    available = 100;
+    test_num = 1;
+    client.query("INSERT INTO test_table (id, priority, available, test_num) VALUES (#{i}, '#{priority_value}', #{available}, #{test_num})")
+end
+
+(204001..206000).each do |i|
+    row = client.query("SELECT * FROM test_table WHERE id = #{i}").first
+    if row
+        client.query("DELETE FROM test_table WHERE id = #{i}")
+    end
+
+    random_number = rand(1..10000)
+    priority_value = "priority#{random_number}"
+    available = 100;
+    test_num = 1;
+    client.query("INSERT INTO test_table (id, priority, available, test_num) VALUES (#{i}, '#{priority_value}', #{available}, #{test_num})")
+end
+
+(206001..208000).each do |i|
+    row = client.query("SELECT * FROM test_table WHERE id = #{i}").first
+    if row
+        client.query("DELETE FROM test_table WHERE id = #{i}")
+    end
+
+    random_number = rand(1..10000)
+    priority_value = "priority#{random_number}"
+    available = 100;
+    test_num = 2;
+    client.query("INSERT INTO test_table (id, priority, available, test_num) VALUES (#{i}, '#{priority_value}', #{available}, #{test_num})")
+end
+
+(2001..202000).each do |i|
+    row = client.query("SELECT * FROM test_table WHERE id = #{i}").first
+    if row
+        client.query("DELETE FROM test_table WHERE id = #{i}")
+    end
+
+    random_number = rand(1..10000)
+    priority_value = "priority##{random_number}"
+    available = 1
+    test_num = 3
+    client.query("INSERT INTO test_table (id, priority, available, test_num) VALUES (#{i}, '#{priority_value}', #{available}, #{test_num})")
+end
